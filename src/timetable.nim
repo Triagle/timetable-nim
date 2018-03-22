21800847ce512fc5219f99c33e10ebe82dfd5957
Jake <jakefaulkn@gmail.com>
2018-03-22 23:58:12 +1300

    First working implementation of timetable

--- src/timetable.nim
+++ src/timetable.nim
@@ -1,6 +1,6 @@
 import
   times, parsecfg, algorithm, os, sequtils, tables, strutils, marshal, httpclient,
-  options, xmltree, htmlparser, streams, strformat, terminal
+  options, xmltree, htmlparser, streams, strformat, terminal, parseopt2, ospaths
 import nimquery
 
 type
@@ -11,12 +11,12 @@ type
 
   Day = enum
     Monday, Tuesday, Wednesday, Thursday, Friday
-
+    #
   Activity = ref object
     id*: string
     course*: string
     name*: string
-    day*: Day
+    day*: WeekDay
     start*: DateTime
     endtime*: DateTime
     location*: seq[Location]
@@ -36,13 +36,8 @@ proc `$`(x: Activity): string =
 proc `$`(x: Course): string =
   "Course($1, $2, $3, $4)" % [x.title, $x.year, $x.semester, $x.activities]
 
-proc `$`(x: Day): string =
-  ["Monday",  "Tuesday",  "Wednesday", "Thursday", "Friday"][ord(x)]
-proc to_day(x: string): Day =
-  ({"Monday": Monday, "Tuesday": Tuesday, "Wednesday": Wednesday, "Thursday": Thursday, "Friday": Friday}.to_table)[x]
-
-# proc to_day(x: int): Day =
-#   [Monday,  Tuesday,  Wednesday, Thursday, Friday][x]
+proc to_day(x: string): WeekDay =
+  ({"Monday": dMon, "Tuesday": dTue, "Wednesday": dWed, "Thursday": dThu, "Friday": dFri}.to_table)[x]
 
 proc parse_activity(course: string, header: XmlNode, data: XmlNode): auto =
   let
@@ -86,16 +81,27 @@ proc group[T; K](arr: seq[T], key: proc (x: T): K {.closure.}): seq[seq[T]] =
       result.add(new_seq[T]())
     result[result.len - 1].add(el)
 
-proc is_allocated(a: Activity, allocated_activities: TableRef[(string, string), string]): auto =
-  allocated_activities.get_or_default((a.course, a.name)) == a.id or a.id.strip(chars={'0'}) == "1"
+proc get_activities(courses: seq[Course]): auto =
+  courses.foldl(concat(a, b.activities), new_seq[Activity]())
 
-proc courses_on(courses: seq[Course], day: Day): auto =
+proc is_allocated(a: Activity, allocated_activities: TableRef[(string, string), string]): auto =
+  let val = allocated_activities.get_or_default((a.course, a.name))
+  if val.len > 0:
+    val == a.id
+  else:
+    a.id.strip(chars={'0'}) == "1"
+
+proc activities_on(courses: seq[Course], day: WeekDay): auto =
   let
-    activities = courses.foldl(concat(a, b.activities), new_seq[Activity]()).filter_it(it.day == day)
+    activities = courses.get_activities.filter_it(it.day == day)
   activities.sorted do (a: Activity, b: Activity) -> auto: cmp(a.start.to_time, b.start.to_time)
 
-proc courses_allocated(courses: seq[Activity], allocated_activities: TableRef[(string, string), string]): auto =
+proc activities_allocated(courses: seq[Activity], allocated_activities: TableRef[(string, string), string]): auto =
   courses.filter_it(it.is_allocated(allocated_activities))
+
+
+proc activities_after(courses: seq[Course], time: DateTime): auto =
+  courses.get_activities.filter_it(it.day == time.weekday and it.start.to_time > time.to_time)
 
 
 proc read_cfg(path: string): auto =
@@ -110,24 +116,76 @@ proc read_cfg(path: string): auto =
         semester = cfg.get_section_value(section, "semester").parse_int
         year = cfg.get_section_value(section, "year").parse_int
       courses.add(Course(title: title, semester: semester, year: year, activities: @[]))
-    elif section.starts_with("activity/"):
+    else:
       let
-        activity_name = section["activity/".len..^1].replace("_", " ")
-        course = cfg.get_section_value(section, "course")
+        tokens = section.split("/")
         selected_activity = cfg.get_section_value(section, "activity")
-      tbl[(course, activity_name)] = selected_activity
+      tbl[(tokens[0], tokens[1].replace("_", " "))] = selected_activity
   (courses: courses, selected_activities: tbl)
 
 
-proc print_courses(day: Day, activities: seq[Activity]) =
+proc print_activity(a: Activity) =
+  let
+    start_string = a.start.format("h:mmtt")
+    end_string = a.endtime.format("h:mmtt")
+    activity_fmt = fmt" {a.name} @ {start_string} - {end_string}"
+  styled_echo(styleDim, a.course, resetStyle, activity_fmt)
+
+proc print_timetable(day: WeekDay, activities: seq[Activity]) =
   styled_echo("Timetable for ", styleDim, $day, resetStyle)
   echo "---"
   for a in activities:
-    let
-      start_string = a.start.format("h:mmtt")
-      end_string = a.endtime.format("h:mmtt")
-      activity_fmt = fmt" {a.name} @ {start_string} - {end_string}"
-    styled_echo(styleDim, a.course, resetStyle, activity_fmt)
+    print_activity(a)
 
-let cfg = read_cfg("./test.cfg")
-print_courses(Friday, map(cfg.courses, get_course).courses_on(Friday).courses_allocated(cfg.selected_activities))
+
+var
+  cur = now()
+  day_filter = cur.weekday
+  next_flag = false
+  show_full = true
+  config_location = "."
+
+for kind, key, val in getopt():
+  case kind
+  of cmdArgument: config_location = key
+  of cmdLongOption, cmdShortOption:
+    case key
+    of "on", "o": day_filter = val.to_day
+    of "next", "n": next_flag = true
+    of "time": show_full = false
+  of cmdEnd: assert(false)
+
+let
+  cfg = read_cfg(config_location / "config.ini")
+  data_location = config_location / "data.json"
+var courses: seq[Course]
+
+if file_exists(data_location):
+  var data = new_file_stream(data_location, fmRead)
+  defer: data.close()
+  load(data, courses)
+else:
+  courses = map(cfg.courses, get_course)
+  var outf = new_file_stream(data_location, fmWrite)
+  defer: outf.close()
+  store(outf, courses)
+
+if next_flag == false:
+  let classes = courses.activities_on(day_filter).activities_allocated(cfg.selected_activities)
+  print_timetable(day_filter, classes)
+else:
+  let next_classes = courses.activities_after(cur).activities_allocated(cfg.selected_activities)
+  if next_classes.len == 0:
+    echo "-"
+  elif show_full:
+    let next_class = next_classes[0]
+    print_activity(next_class)
+  else:
+    let
+      next_class = next_classes[0]
+      interval = next_class.start.to_time.to_time_interval - cur.to_time.to_time_interval
+      seconds = interval.hours * 3600 + interval.minutes * 60
+      hours = seconds div 3600
+      minutes = (seconds - hours * 3600) div 60
+      start_str = ($minutes).align(2, '0')
+    echo fmt"{hours}:{start_str}"
